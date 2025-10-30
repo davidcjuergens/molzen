f7ff34fec947cbb8c4638bd9a60aec47480e6a81
David Juergens <davidcj@stanford.edu>
2025-10-30 15:32:06 -0700

    update constants

--- molzen/constants.py
+++ molzen/constants.py
@@ -1,30 +1,25 @@
-# fmt: off
 # Conversion of atomic units to SI units (Szabo and Ostlund, pg. 42)
-a_0     = 5.2918e-11        # length: Bohr radius (meters)
-m_e     = 9.1095e-31        # mass: electron mass (kg)
-e       = 1.6022e-19        # charge: elementary charge (C)
-eps_a   = 4.359744722e-18   # energy: Hartree (J)
-hbar    = 1.0546e-34        # angular momentum: reduced Planck constant (J*s), for higher accuracy use https://physics.nist.gov/cgi-bin/cuu/Value?hbar
-ea_0    = e * a_0           # electric dipole moment (C*m)
+BOHR_RADIUS_M = 5.2918e-11  # length: Bohr radius (meters)
+ELECTRON_MASS_KG = 9.1095e-31  # mass: electron mass (kg)
+ELEMENTARY_CHARGE = 1.6022e-19  # charge: elementary charge (C)
+JOULE_PER_HARTREE = 4.359744722e-18  # energy: Hartree (J)
+HBAR = 1.0546e-34  # angular momentum: reduced Planck constant (J*s), for higher accuracy use https://physics.nist.gov/cgi-bin/cuu/Value?hbar
+EA_0 = ELEMENTARY_CHARGE * BOHR_RADIUS_M  # electric dipole moment (C*m)
 
-bohr_radius         = a_0
-electron_mass       = m_e
-elementary_charge   = e
-hartree             = eps_a
 
-a_0_angstrom        = a_0 * 1e10  # length: Bohr radius (Angstrom)
-angstrom2bohr       = 1 / a_0_angstrom
+A_0_ANGSTROM = BOHR_RADIUS_M * 1e10  # length: Bohr radius (Angstrom)
+ANGSTROM2BOHR = 1 / A_0_ANGSTROM
 
 # energy conversions
-hartree2kcalmol    = 627.5 # from: http://wild.life.nctu.edu.tw/class/common/energy-unit-conv-table.html
-ev2kcalmol         = 23.06
-hartree2ev          = 27.2107
-hartree2mev         = hartree2ev * 1000
+HARTREE2KCALMOL = (
+    627.5  # from: http://wild.life.nctu.edu.tw/class/common/energy-unit-conv-table.html
+)
+EV2KCALMOL = 23.06
+HARTREE2EV = 27.2107
+HARTREE2MEV = HARTREE2EV * 1000
 
 # time
-au_time = hbar / hartree
-fs_per_s = 1e+15
-fs_per_au_time = fs_per_s * au_time 
-au_time_per_fs = 1 / fs_per_au_time
-
-# fmt: on
+AU_TIME = HBAR / JOULE_PER_HARTREE  # atomic unit of time in seconds
+FS_PER_S = 1e15
+FS_PER_AU_TIME = FS_PER_S * AU_TIME
+AU_TIME_PER_FS = 1 / FS_PER_AU_TIME
