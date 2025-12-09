ddc048580633b3c008d3096cdd89e82f2b8c9df1
David Juergens <davidcj@stanford.edu>
2025-12-08 17:32:20 -0800

    add kabsch algo

--- molzen/kinematics.py
+++ molzen/kinematics.py
@@ -1,4 +1,58 @@
-import numpy as np 
+import numpy as np
+
+
+def np_kabsch(A, B):
+    """
+    Numpy version of kabsch algorithm. Superimposes B onto A
+
+    Parameters:
+        (A,B) np.array - shape (N,3) arrays of xyz crds of points
+
+
+    Returns:
+        rms - rmsd between A and B
+        R - rotation matrix to superimpose B onto A
+        rB - the rotated B coordinates
+    """
+    A = np.copy(A)
+    B = np.copy(B)
+
+    def centroid(X):
+        # return the mean X,Y,Z down the atoms
+        return np.mean(X, axis=0, keepdims=True)
+
+    def rmsd(V, W, eps=0):
+        # First sum down atoms, then sum down xyz
+        N = V.shape[-2]
+        return np.sqrt(np.sum((V - W) * (V - W), axis=(-2, -1)) / N + eps)
+
+    N, ndim = A.shape
+
+    # move to centroid
+    A = A - centroid(A)
+    B = B - centroid(B)
+
+    # computation of the covariance matrix
+    C = np.matmul(A.T, B)
+
+    # compute optimal rotation matrix using SVD
+    U, S, Vt = np.linalg.svd(C)
+
+    # ensure right handed coordinate system
+    d = np.eye(3)
+    d[-1, -1] = np.sign(np.linalg.det(Vt.T @ U.T))
+
+    # construct rotation matrix
+    R = Vt.T @ d @ U.T
+
+    # get rotated coords
+    rB = B @ R
+
+    # calculate rmsd
+    rms = rmsd(A, rB)
+
+    return rms, rB, R
+
 
 def rotate_around_dihedral(
     xyz: np.ndarray, dih_idx: list, atom_idx: list, angle_degrees: float
