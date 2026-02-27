44758d7260dfbbba3d4b46bcc23c0b007bdeb792
David Juergens <davidcj@stanford.edu>
2026-02-26 17:52:31 -0800

    ruff

--- tests/test_package_structure.py
+++ tests/test_package_structure.py
@@ -1,6 +1,8 @@
 """Testing imports / deployment / structure of the package."""
 
+
 def test_imports():
     """Test some namespaces after importing."""
     import molzen as mz
+
     assert hasattr(mz, "io")
